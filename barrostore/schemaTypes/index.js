fdc20457cfa8bbce284d6c4c7ac3ea9fe8686ac8
severo <oliveira.severo@gmail.com>
2024-04-04 11:09:43 -0300

    feat: :card_file_box: Corrige schema de produto

--- barrostore/schemaTypes/index.js
+++ barrostore/schemaTypes/index.js
@@ -2,6 +2,6 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
-import products from './products'
+import product from './product'
 
-export const schemaTypes = [post, author, category, blockContent, products]
+export const schemaTypes = [post, author, category, blockContent, product]
