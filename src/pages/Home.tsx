f4b94253e97f6390121ab094e0f89b0b77f57ec8
severo <oliveira.severo@gmail.com>
2024-04-05 19:49:39 -0300

    feat: :rocket: Cria schemas para páginas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,24 @@
 function Home() {
   return (
-    <h1>Home page</h1>
+    <>
+      <section className="hero">
+        <figure>FOTO IMAGEM DESTAQUE</figure>
+        <div className="hero_container_logo">
+          <p>Vasos, Cerâmicas e Plantas</p>
+          <h1>BARRO</h1>
+          <div className="social_links">
+            <ul>
+              <li>Localizacao</li>
+              <li>WhatsApp</li>
+              <li>Instagram</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <section className="second_section">
+        fdfdfdff
+      </section>
+    </>
   );
 }
 
