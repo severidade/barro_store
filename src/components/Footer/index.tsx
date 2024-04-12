327dfc3935be31363319bf873a1a42fe41afabe1
severo <oliveira.severo@gmail.com>
2024-04-12 11:42:20 -0300

    feat: :lipstick: Ajustes no CSS

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ import styles from './footer.module.css';
 
 function Footer() {
   return (
-    <footer className={ styles.foter }>
+    <footer className={ styles.footer }>
       <p>Rua das Flores, 73 - Boa Vista</p>
       <p> Jaboticatubas | MG</p>
     </footer>
