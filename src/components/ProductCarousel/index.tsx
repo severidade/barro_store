d3a6e70cd4c7fa526d678bfe8b7a3f5aee255d0b
severo <oliveira.severo@gmail.com>
2024-04-14 12:34:57 -0300

    feat: :rocket: Implementa navegaçao em carrosel por imagens.

--- src/components/ProductCarousel/index.tsx
+++ src/components/ProductCarousel/index.tsx
@@ -1,6 +1,7 @@
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import CustomPaging from './CustomPaging';
 import { urlFor } from '../../utils/buildSanityImageUrl';
 import './ProductCarousel.css';
 
@@ -17,6 +18,7 @@ function ProductCarousel({ images, name }: ProductCarouselProps) {
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
+    customPaging: CustomPaging(images),
   };
 
   return (
