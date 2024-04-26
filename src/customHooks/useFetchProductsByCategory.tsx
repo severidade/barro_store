98231257979adf696e428f681243d45a6274ffa7
severo <oliveira.severo@gmail.com>
2024-04-26 18:14:24 -0300

    feat: :rocket: Modifica useFetchProductsByCategory para receber somente o id
    Modifica também a página products para passar somente essa informação

--- src/customHooks/useFetchProductsByCategory.tsx
+++ src/customHooks/useFetchProductsByCategory.tsx
@@ -1,18 +1,18 @@
 import { useEffect, useState } from 'react';
 import { fetchProductsByCategory } from '../utils/fetch';
 import { Product } from '../types/Product';
-import { Category } from '../types/Category';
+// import { Category } from '../types/Category';
 
 // eslint-disable-next-line max-len
-function useFetchProductsByCategory(categoryDetails: Category | null) {
+function useFetchProductsByCategory(categoryId: string | null) {
   const [productsByCategory, setProductsByCategory] = useState<Product[] | null>(null);
 
   useEffect(() => {
     async function fetchAllProductsByCategory() {
       try {
-        if (categoryDetails) {
+        if (categoryId) {
           // eslint-disable-next-line no-underscore-dangle
-          const data = await fetchProductsByCategory(categoryDetails._id);
+          const data = await fetchProductsByCategory(categoryId);
           setProductsByCategory(data);
         }
       } catch (error) {
@@ -21,9 +21,11 @@ function useFetchProductsByCategory(categoryDetails: Category | null) {
     }
 
     fetchAllProductsByCategory();
-  }, [categoryDetails]);
+  }, [categoryId]);
 
   return productsByCategory;
 }
 
 export default useFetchProductsByCategory;
+
+// com o id da categoria eu busco todos os produtos da categoria
