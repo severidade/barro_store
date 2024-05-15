74e82b23dbbad66cef2319e8cd4bbb2d55701631
severo <oliveira.severo@gmail.com>
2024-05-15 11:30:57 -0300

    feat: :rocket: produtos por categoria carrosel scroll infinito

--- src/components/ProductsByCategoryCarousel/index.tsx
+++ src/components/ProductsByCategoryCarousel/index.tsx
@@ -25,7 +25,7 @@ interface FetchProductsByCategoryProps {
 function ProductsByCategoryCarousel({ categoryId, categoryOfProduct, productId } : FetchProductsByCategoryProps) {
   const settings = {
     dots: true,
-    infinite: false,
+    infinite: true,
     speed: 400,
     slidesToShow: 3,
     slidesToScroll: 2,
