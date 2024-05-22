62bcbb5699d3bf662ed954beb240cc3ed4414bab
severo <oliveira.severo@gmail.com>
2024-05-22 15:36:09 -0300

    feat: :rocket: home hero open

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,21 @@
+import HighlightImage from '../HighlightImage';
+import HomeBranding from '../HomeBranding';
+import styles from './hero.module.css';
+
+interface HeroProps {
+  url: string;
+}
+
+function Hero({ url }: HeroProps) {
+  return (
+    <div className={ styles.container_hero }>
+      <HighlightImage
+        imageUrl={ url }
+        imageType="hero"
+      />
+      <HomeBranding />
+    </div>
+  );
+}
+
+export default Hero;
