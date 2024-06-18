ea93d1e042aa7cdb48396d1aab51ae23b892e524
severo <oliveira.severo@gmail.com>
2024-06-18 16:48:29 -0300

    feat: :rocket: Conecta o Store ao Aplicativo

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App.tsx';
+import { Provider } from 'react-redux';
+import App from './App';
+import { store } from './redux/store'; // Importando a store do Redux
 import './CSS/reset.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}> {/* Envolvendo o App com o Provider */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 );
