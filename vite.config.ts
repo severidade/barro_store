3e8d0b6d6c345ed75239df37c42620f57a46852f
severo <oliveira.severo@gmail.com>
2025-03-21 17:44:53 -0300

    feat: :rocket: Atualizando sintaxe

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // vite.config.js
-// export default {
-//   server: {
-//     host: '0.0.0.0', // Aceita conexões de qualquer IP
-//     port: 5173,
-//   },
-// };
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
