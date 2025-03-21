07231cd64b7a250b6ee98c79b63371347b206448
severo <oliveira.severo@gmail.com>
2025-03-21 17:32:38 -0300

    feat: :rocket: Configura vitconfig para permitr acecsso ao localhost para dispositivos na rede

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// vite.config.js
+export default {
+  server: {
+    host: '0.0.0.0', // Aceita conexões de qualquer IP
+    port: 5173,
+  },
+};
