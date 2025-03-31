d61cf4b6c28a6783659d1755367badc71865d250
severo <oliveira.severo@gmail.com>
2025-03-31 18:49:14 -0300

    style: :lipstick: Estilos para FavoriteProductList

--- src/components/FavoriteProductList/index.tsx
+++ src/components/FavoriteProductList/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable max-len */
 import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';
+import styles from './FavoriteProductList.module.css';
 import FavoriteProductCard from '../FavoriteProductCard';
 
 function FavoriteProductList() {
@@ -10,24 +11,8 @@ function FavoriteProductList() {
     return state.favoriteProducts;
   });
 
-  // return (
-  //   <>
-  //     {favoriteProducts.length > 0 ? (
-  //       <ul className="favorite-products-list">
-  //         {favoriteProducts.map((product) => (
-  //           <FavoriteProductCard
-  //             product={ product }
-  //             key={ product.id }
-  //           />
-  //         ))}
-  //       </ul>
-  //     ) : (
-  //       <p>Você ainda não adicionou produtos à sua lista de favoritos.</p>
-  //     )}
-  //   </>
-  // );
   return favoriteProducts.length > 0 ? (
-    <ul className="favorite-products-list">
+    <ul className={ styles.favorite_products_list }>
       {favoriteProducts.map((product) => (
         <FavoriteProductCard product={ product } key={ product.id } />
       ))}
